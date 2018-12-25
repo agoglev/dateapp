0c6c37c246a7ba877ac1872c7bc0ac7166527308
goglev <goglev-andrey@mail.ru>
2018-12-25 18:31:28 +0300

    autoreg for direct games

--- src/containers/Join/JoinIntro.js
+++ src/containers/Join/JoinIntro.js
@@ -9,6 +9,7 @@ import * as utils from '../../utils';
 import Header from '../../components/proxy/Header';
 import * as accountActions from "../../actions/account";
 import * as api from '../../services/api';
+import * as loadImage from "blueimp-load-image";
 
 export default class JoinIntro extends BaseComponent {
   componentDidMount() {
@@ -45,13 +46,84 @@ export default class JoinIntro extends BaseComponent {
       api.vk('users.get', {
         fields: 'sex,bdate,country,city,photo_max'
       }).then((users) => {
-        actions.loaderHide();
-        console.log(users);
-        accountActions.setupVkInfo(users[0]);
-        setTimeout(actions.openJoinStep1, 100);
-      }).catch(() => actions.showError());
+        const user = users[0];
+        accountActions.setupVkInfo(user);
+        if (this._isCanAutoReg(user)) {
+          this._autoReg(user);
+        } else {
+          actions.loaderHide();
+          setTimeout(actions.openJoinStep1, 100);
+        }
+      }).catch((err) => actions.showError(err.message));
     } else {
       actions.openJoinStep1();
     }
   };
+
+  _isCanAutoReg(user) {
+    return (
+      user.first_name &&
+      user.bdate &&
+      user.sex &&
+      user.country &&
+      user.city &&
+      user.photo_max &&
+      user.photo_max.indexOf('camera_') === -1 &&
+      user.photo_max.indexOf('/images/') === -1
+    );
+  }
+
+  _autoReg(user) {
+    this._uploadPhoto(user.photo_max).then((photoHash) => {
+      const bdate = (user.bdate || '').split('.').map((item) => parseInt(item, 10));
+      const day = bdate[0] || 0;
+      const month = Math.max(0, (bdate[1] - 1)) || 0;
+      const year = bdate[2] || 0;
+
+      accountActions.fillJoinInfo({
+        name: user.first_name,
+        gender: user.sex,
+        day,
+        month,
+        year,
+        country: user.country ? user.country.id : 0,
+        city: user.city ? user.city.id : 0
+      });
+      accountActions.createAccount([photoHash]);
+    }).catch((err) => actions.showError(err.message));
+  }
+
+  _uploadPhoto(src) {
+    return new Promise((resolve, reject) => {
+      fetch(src)
+        .then(res => res.blob())
+        .then(file => {
+          file.lastModifiedDate = new Date();
+
+          loadImage.parseMetaData(file, (data) => {
+            let orientation = 0;
+            if (data.exif) {
+              orientation = data.exif.get('Orientation');
+            }
+            loadImage(
+              file,
+              (canvas) => {
+                const base64data = canvas.toDataURL('image/jpeg');
+                const blobBin = atob(base64data.split(',')[1]);
+                let array = [];
+                for(let i = 0; i < blobBin.length; i++) {
+                  array.push(blobBin.charCodeAt(i));
+                }
+                const newFile = new Blob([new Uint8Array(array)], {type: 'image/png'});
+                actions.uploadPhoto(newFile).then((photoHash) => resolve(photoHash)).catch(reject);
+              }, {
+                canvas: true,
+                orientation: orientation,
+                maxWidth: 800
+              }
+            );
+          });
+        }).catch(reject);
+    });
+  }
 }
